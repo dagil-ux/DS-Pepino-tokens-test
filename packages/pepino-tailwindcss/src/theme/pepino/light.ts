b3dc73d4d32bc26e33e9d5a7d89faa02eb765204
dagil-ux <david.gil@f2x.com.co>
2026-06-30 09:37:41 -0500

    chore(tokens): v1.30

---
+++ packages/pepino-tailwindcss/src/theme/pepino/light.ts
@@ -0,0 +1,126 @@
+export const PepinoLightTheme = {
+  /* -------------------------------------------------------------------------- */
+  /* Background colors                                                          */
+  /* -------------------------------------------------------------------------- */
+  'pp-background--alt': 'var(--pp-neutral-100)',
+  'pp-background--brand': 'var(--pp-brand)',
+  'pp-background--inverse': 'var(--pp-neutral-1500)',
+  'pp-background--overlay': 'rgba(31, 31, 31, 0.8)',
+  'pp-background--primary': 'var(--pp-neutral-50)',
+  'pp-background--secondary': 'var(--pp-neutral-200)',
+  'pp-background--tertiary': 'var(--pp-neutral-300)',
+  /* -------------------------------------------------------------------------- */
+  /* Icons colors                                                               */
+  /* -------------------------------------------------------------------------- */
+  'pp-icon--alt': 'var(--pp-neutral-50)',
+  'pp-icon--brand': 'var(--pp-brand)',
+  'pp-icon--danger': 'var(--pp-danger)',
+  'pp-icon--info': 'var(--pp-info-dark)',
+  'pp-icon--inverse': 'var(--pp-neutral-50)',
+  'pp-icon--primary': 'var(--pp-neutral-1500)',
+  'pp-icon--secondary': 'var(--pp-neutral-800)',
+  'pp-icon--success': 'var(--pp-success)',
+  'pp-icon--warining': 'var(--pp-warning)',
+  /* -------------------------------------------------------------------------- */
+  /* Stroke colors                                                              */
+  /* -------------------------------------------------------------------------- */
+  'pp-stroke--alt': 'var(--pp-neutral-500)',
+  'pp-stroke--brand': 'var(--pp-brand)',
+  'pp-stroke--brand-alt': 'var(--pp-brand-light)',
+  'pp-stroke--danger': 'var(--pp-danger)',
+  'pp-stroke--info': 'var(--pp-info-dark)',
+  'pp-stroke--inverse': 'var(--pp-neutral-700)',
+  'pp-stroke--primary': 'var(--pp-neutral-500)',
+  'pp-stroke--secondary': 'var(--pp-neutral-300)',
+  'pp-stroke--success': 'var(--pp-success-dark)',
+  'pp-stroke--tertiary': 'var(--pp-neutral-400)',
+  'pp-stroke--warning': 'var(--pp-warning)',
+  /* -------------------------------------------------------------------------- */
+  /* Text colors                                                                */
+  /* -------------------------------------------------------------------------- */
+  'pp-text--alt': 'var(--pp-neutral-800)',
+  'pp-text--brand': 'var(--pp-brand)',
+  'pp-text--danger': 'var(--pp-danger)',
+  'pp-text--dangr-alt': 'var(--pp-danger)',
+  'pp-text--info': 'var(--pp-info-dark)',
+  'pp-text--info-alt': 'var(--pp-info)',
+  'pp-text--inverse': 'var(--pp-neutral-50)',
+  'pp-text--primary': 'var(--pp-neutral-1500)',
+  'pp-text--secondary': 'var(--pp-neutral-900)',
+  'pp-text--success': 'var(--pp-success-dark)',
+  'pp-text--success-alt': 'var(--pp-success)',
+  'pp-text--warning': 'var(--pp-warning)',
+  'pp-text--warning-alt': 'var(--pp-warning)',
+  /* -------------------------------------------------------------------------- */
+  /* UI colors                                                                  */
+  /* -------------------------------------------------------------------------- */
+  'pp-ui--alt': 'var(--pp-neutral-100)',
+  'pp-ui--brand': 'var(--pp-brand)',
+  'pp-ui--brand-alt': 'var(--pp-brand-light)',
+  'pp-ui--danger': 'var(--pp-danger)',
+  'pp-ui--danger-alt': 'var(--pp-danger-light)',
+  'pp-ui--info': 'var(--pp-info)',
+  'pp-ui--info-alt': 'var(--pp-info-light)',
+  'pp-ui--inverse': 'var(--pp-neutral-1300)',
+  'pp-ui--primary': 'var(--pp-neutral-50)',
+  'pp-ui--secondary': 'var(--pp-neutral-200)',
+  'pp-ui--success': 'var(--pp-success)',
+  'pp-ui--success-alt': 'var(--pp-success-light)',
+  'pp-ui--tertiary': 'var(--pp-neutral-300)',
+  'pp-ui--warning': 'var(--pp-warning)',
+  'pp-ui--warning-alt': 'var(--pp-warning-light)',
+  /* -------------------------------------------------------------------------- */
+  /* Services                                                                   */
+  /* -------------------------------------------------------------------------- */
+  'pp-services--carwash': 'var(--pp-carwash)',
+  'pp-services--carwash-alt': 'var(--pp-carwash-light)',
+  'pp-services--fuel': 'var(--pp-fuel)',
+  'pp-services--fuel-alt': 'var(--pp-fuel-light)',
+  'pp-services--parking': 'var(--pp-parking)',
+  'pp-services--parking-alt': 'var(--pp-parking-light)',
+  'pp-services--parkingmeter': 'var(--pp-parkingmeter)',
+  'pp-services--parkingmeter-alt': 'var(--pp-parkingmeter-light)',
+  'pp-services--toll': 'var(--pp-toll)',
+  'pp-services--toll-alt': 'var(--pp-toll-light)',
+  /* -------------------------------------------------------------------------- */
+  /* Button                                                                     */
+  /* -------------------------------------------------------------------------- */
+  'pp-button--bg': 'var(--pp-lime-500)',
+  'pp-button--bg-alt': 'var(--pp-lime-100)',
+  'pp-button--bg-alt-hover': 'var(--pp-lime-200)',
+  'pp-button--bg-alt-press': 'var(--pp-lime-300)',
+  'pp-button--bg-destructive': 'var(--pp-red-300)',
+  'pp-button--bg-destructive-hover': 'var(--pp-red-400)',
+  'pp-button--bg-destructive-press': 'var(--pp-red-500)',
+  'pp-button--bg-disabled': 'var(--pp-gray-100)',
+  'pp-button--bg-ghost-hover': 'var(--pp-background--tertiary)',
+  'pp-button--bg-hover': 'var(--pp-lime-600)',
+  'pp-button--bg-press': 'var(--pp-lime-700)',
+  'pp-button--border': 'var(--pp-button--bg)',
+  'pp-button--border-destructive': 'var(--pp-red-500)',
+  'pp-button--border-destructive-hover': 'var(--pp-red-600)',
+  'pp-button--border-destructive-press': 'var(--pp-red-700)',
+  'pp-button--border-disabled': 'var(--pp-button--bg-disabled)',
+  'pp-button--border-hover': 'var(--pp-button--bg-hover)',
+  'pp-button--border-press': 'var(--pp-button--bg-press)',
+  'pp-button--txt': 'var(--pp-text--primary)',
+  'pp-button--txt-disabled': 'var(--pp-text--secondary)',
+  'pp-button--txt-primary': 'var(--pp-dark-gray-900)',
+  /* -------------------------------------------------------------------------- */
+  /* Alertas                                                                    */
+  /* -------------------------------------------------------------------------- */
+  'pp-alertas--advertencia-background': 'var(--pp-orange-100)',
+  'pp-alertas--advertencia-texto': 'var(--pp-orange-500)',
+  'pp-alertas--error-background': 'var(--pp-red-100)',
+  'pp-alertas--error-texto': 'var(--pp-red-600)',
+  'pp-alertas--éxito-background': 'var(--pp-green-100)',
+  'pp-alertas--éxito-texto': 'var(--pp-green-800)',
+  'pp-alertas--informativo-background': 'var(--pp-blue-100)',
+  'pp-alertas--informativo-texto': 'var(--pp-blue-800)',
+  /* --- Extra tokens --- */
+  // !Note: temp tokens states
+  'pp-active': 'rgba(39, 39, 39, 0.2)',
+  'pp-disabled': 'var(--pp-neutral-300)',
+  'pp-focus': 'var(--pp-accent-dark)',
+  'pp-hover': 'rgba(118, 118, 118, 0.2)',
+};
