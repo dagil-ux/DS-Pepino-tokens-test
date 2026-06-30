0caed8774be06113fbef232790ab947cef57a9ef
dagil-ux <david.gil@f2x.com.co>
2026-06-30 09:37:40 -0500

    chore(tokens): v1.30

---
+++ packages/pepino-tailwindcss/src/theme/pepino/root.ts
@@ -0,0 +1,114 @@
+export const PepinoRootTheme = {
+  /** White / Black */
+  'pp-black': 'var(--pp-dark-gray-900)',
+  'pp-white': 'var(--pp-white-100)',
+  /** Neutrals */
+  'pp-neutral-100': 'var(--pp-white-200)',
+  'pp-neutral-1000': 'var(--pp-dark-gray-400)',
+  'pp-neutral-1100': 'var(--pp-gray-800)',
+  'pp-neutral-1200': 'var(--pp-dark-gray-600)',
+  'pp-neutral-1300': 'var(--pp-dark-gray-700)',
+  'pp-neutral-1400': 'var(--pp-dark-gray-800)',
+  'pp-neutral-1500': 'var(--pp-dark-gray-900)',
+  'pp-neutral-200': 'var(--pp-white-300)',
+  'pp-neutral-300': 'var(--pp-white-400)',
+  'pp-neutral-400': 'var(--pp-white-500)',
+  'pp-neutral-50': 'var(--pp-white-100)',
+  'pp-neutral-500': 'var(--pp-gray-100)',
+  'pp-neutral-600': 'var(--pp-gray-400)',
+  'pp-neutral-700': 'var(--pp-dark-gray-200)',
+  'pp-neutral-800': 'var(--pp-dark-gray-300)',
+  'pp-neutral-900': 'var(--pp-gray-500)',
+  'pp-neutral-950': 'var(--pp-gray-600)',
+  /** Brand */
+  'pp-brand': 'var(--pp-lime-500)',
+  'pp-brand-dark': 'var(--pp-lime-900)',
+  'pp-brand-light': 'var(--pp-lime-100)',
+  /** Accent */
+  'pp-accent': 'var(--pp-lime-500)',
+  'pp-accent-dark': 'var(--pp-lime-900)',
+  'pp-accent-light': 'var(--pp-lime-100)',
+  /** Success */
+  'pp-success': 'var(--pp-green-600)',
+  'pp-success-dark': 'var(--pp-green-800)',
+  'pp-success-light': 'var(--pp-green-100)',
+  /** Danger */
+  'pp-danger': 'var(--pp-red-600)',
+  'pp-danger-dark': 'var(--pp-red-900)',
+  'pp-danger-light': 'var(--pp-red-100)',
+  /** Warning */
+  'pp-warning': 'var(--pp-orange-500)',
+  'pp-warning-dark': 'var(--pp-orange-600)',
+  'pp-warning-light': 'var(--pp-orange-100)',
+  /** Info */
+  'pp-info': 'var(--pp-blue-600)',
+  'pp-info-dark': 'var(--pp-blue-800)',
+  'pp-info-light': 'var(--pp-blue-100)',
+  /** Focus */
+  'pp-focus': 'var(--pp-lime-900)',
+  /** Product / Services */
+  'pp-carwash': 'var(--pp-violet-500)',
+  'pp-carwash-dark': 'var(--pp-violet-700)',
+  'pp-carwash-light': 'var(--pp-violet-200)',
+  'pp-fuel': 'var(--pp-salmon-500)',
+  'pp-fuel-dark': 'var(--pp-salmon-700)',
+  'pp-fuel-light': 'var(--pp-salmon-200)',
+  'pp-parking': 'var(--pp-skyblue-500)',
+  'pp-parking-dark': 'var(--pp-skyblue-700)',
+  'pp-parking-light': 'var(--pp-skyblue-200)',
+  'pp-parkingmeter': 'var(--pp-iris-500)',
+  'pp-parkingmeter-dark': 'var(--pp-iris-700)',
+  'pp-parkingmeter-light': 'var(--pp-iris-200)',
+  'pp-toll': 'var(--pp-yellow-500)',
+  'pp-toll-dark': 'var(--pp-yellow-700)',
+  'pp-toll-light': 'var(--pp-yellow-200)',
+
+  /** Spacing */
+  'pp-spacing-xs3': '0.125rem', // 2px
+  'pp-spacing-xs2': '0.25rem', // 4px
+  'pp-spacing-xs': '0.5rem', // 8px
+  'pp-spacing-m': '1rem', // 16px
+  'pp-spacing-xl': '1.5rem', // 24px
+  'pp-spacing-xxl': '2rem', // 32px
+  'pp-spacing-xxl2': '2.5rem', // 40px
+  'pp-spacing-xxl3': '2.75rem', // 44px
+  'pp-spacing-xxl4': '3rem', // 48px
+  'pp-spacing-xxl6': '4rem', // 64px
+  'pp-spacing-xxl7': '5rem', // 80px
+  'pp-spacing-xxl8': '6rem', // 96px
+  'pp-spacing-xxl9': '9rem', // 144px
+  'pp-spacing-s': '0.75rem', // 12px
+  'pp-spacing-l': '1.25rem', // 20px
+  'pp-spacing-none': '0rem', // 0px
+  'pp-spacing-xxl5': '3.5rem', // 56px
+
+  /** Border Radius */
+  'pp-radius--none': '0rem', // 0px
+  'pp-radius--xs2': '0.25rem', // 4px
+  'pp-radius--xs': '0.375rem', // 6px
+  'pp-radius--s': '0.5rem', // 8px
+  'pp-radius--m': '0.625rem', // 10px
+  'pp-radius--l': '1.25rem', // 20px
+  'pp-radius--full': '9999px',
+
+  /** Typography & Other */
+  'pp-font-family-inter': 'Inter',
+  'pp-font-size-xs': '0.625rem', // 10px
+  'pp-font-size-s': '0.75rem', // 12px
+  'pp-font-size-m': '0.875rem', // 14px
+  'pp-font-size-l': '1rem', // 16px
+  'pp-font-size-xl': '1.125rem', // 18px
+  'pp-font-size-xxl': '1.25rem', // 20px
+  'pp-font-size-xxl9': '6rem', // 96px
+  'pp-font-size-xxl8': '5rem', // 80px
+  'pp-font-size-xxl7': '4.5rem', // 72px
+  'pp-font-size-xxl6': '4rem', // 64px
+  'pp-font-size-xxl5': '3rem', // 48px
+  'pp-font-size-xxl4': '2.25rem', // 36px
+  'pp-font-size-xxl3': '1.875rem', // 30px
+  'pp-font-size-xxl2': '1.5rem', // 24px
+  'pp-component-padding-pp-component_padding-s': '0.625rem', // 10px
+  'pp-component-padding-pp-component_padding-m': '0.6875rem', // 11px
+  'pp-component-padding-pp-component_padding-l': '0.75rem', // 12px
+  'pp-font-size-xs2': '0.5rem', // 8px
+};
